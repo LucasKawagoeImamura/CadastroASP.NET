abb49c0c582b067fb479c768c06e2ac4dfbeca16
Lucas <lucas.imamura@gmail.com>
2025-05-09 15:03:01 -0300

    Configurando o Banco de Dados mysql

---
+++ CadastroASP.NET/Script.sql
@@ -0,0 +1,14 @@
+create database dbcadastro;
+use dbcadastro;
+create table Usuarios(
+Id int primary key,
+Nome varchar(200) not null,
+Email varchar(200) not null,
+Senha varchar(50) not null);
+
+create table Produtos(
+Id int primary key,
+Nome varchar(200) not null,
+Descricao varchar(200) not null,
+Preco decimal(10,2) not null,
+Quantidade int not null);
